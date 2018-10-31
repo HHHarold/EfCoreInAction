e2b814a9fbf4b8eefb57578bcc137d581b6a7663
Harold Hernandez <hhernandez@belatrixsf.com>
2018-10-31 16:47:11 -0500

    progress

--- Chapter2/MyFirstEfCoreApp/MyFirstEfCoreApp/EfCoreContext.cs
+++ Chapter2/MyFirstEfCoreApp/MyFirstEfCoreApp/EfCoreContext.cs
@@ -8,5 +8,10 @@ namespace MyFirstEfCoreApp
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<PriceOffer> PriceOffers { get; set; }
+
+        public EfCoreContext (DbContextOptions<EfCoreContext> options)
+            : base (options) { }
+
+        protected override void OnModelCreating (ModelBuilder model) { }
     }
 }
